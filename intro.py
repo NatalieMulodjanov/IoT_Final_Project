1eae603fc51191310f1ba627f83d4a3b91014727
Mankirat Singh Sarwara <sarwara.mankirat@gmail.com>
2022-03-24 23:23:44 -0400

    Added DHT11 data communication, Data Reception, and Displaying Humidity and Temp in Gauges.

--- intro.py
+++ intro.py
@@ -2,10 +2,18 @@ import dash
 from dash import dcc, html
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
+from DHT11 import loop
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+
+# Starting the application.
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+#Importing and cleaning data to be used.
+
+
+#Layout of the application (Dash components, HTML).
 app.layout = html.Div([
     dcc.Interval(
         id='interval-component',
@@ -23,46 +31,42 @@ app.layout = html.Div([
 
 
 # Multiple components can update everytime interval gets fired.
-@app.callback(Output('live-update-graph-temp', 'figure'),
-              Input('interval-component', 'n_intervals'))
+@app.callback(
+        [Output('live-update-graph-temp', 'figure'),
+         Output('live-update-graph-humidity','figure')],
+        [Input('interval-component', 'n_intervals')])
+
 def update_graph_live(n):
-    temps = [1, 4, 6, 9, 15, 30, 60, 65.70, 100,
-             120, 100, 80, 60, 40, 30, 20, 10, 5, 1]
+
     # Collect some data
+    data = loop()
+    humidity = data[0]
+    temperature = data[1]
 
     # Create the graph with subplots
-    fig = go.Figure(go.Indicator(
+
+    # Humidity Gauge
+    humidityFig = go.Figure(go.Indicator(
         mode="gauge+number",
-        value=temps[n%len(temps)],
-                            gauge={'axis': {'range': [-2, 11]},
-                        'steps': [
-                            {'range': [-2, 0], 'color': "white"},
-                            {'range': [0, 7], 'color': "white"},
-                            {'range': [7, 11], 'color': "white"}],
-                        'threshold': {'line': {'color': "black", 'width': 1}, 'thickness': 1, 'value': 7}},
+        value=humidity,
+        domain={'x': [0, 1], 'y': [0, 1]},
+        title={'text': "Humidity"}))
+
+    # Temperature Gauge
+    temperatureFig = go.Figure(go.Indicator(
+        mode="gauge+number",
+        value=temperature,
+                         #   gauge={'axis': {'range': [-2, 11]},
+                        #'steps': [
+                         #   {'range': [-2, 0], 'color': "white"},
+                          #  {'range': [0, 7], 'color': "white"},
+                           # {'range': [7, 11], 'color': "white"}],
+                        #'threshold': {'line': {'color': "black", 'width': 1}, 'thickness': 1, 'value': 7}},
         # width=200,
         domain={'x': [0, 1], 'y': [0, 1]},
         title={'text': "Temperature"}))
 
-    return fig
-
-
-@app.callback(Output('live-update-graph-humidity', 'figure'),
-              Input('interval-component', 'n_intervals'))
-def update_graph_live(n):
-    humidity = [1, 4, 6, 9, 15, 30, 60, 65.70, 100,
-                120, 100, 80, 60, 40, 30, 20, 10, 5, 1]
-    # Collect some data
-
-    # Create the graph with subplots
-    fig = go.Figure(go.Indicator(
-        mode="gauge+number",
-        value=humidity[n%len(humidity)],
-        domain={'x': [0, 1], 'y': [0, 1]},
-        title={'text': "Humidity"}))
-
-    return fig
-
+    return temperatureFig,humidityFig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
